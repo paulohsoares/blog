95f0cca3e49d636e4a5b272444a2d07978dd8873
paulohsoares <don.psoares@outlook.com>
2022-08-10 13:21:02 +0000

    Adicionando codigo HTML ao retorno da funcao de contatos.

--- app.py
+++ app.py
@@ -7,5 +7,17 @@ def hello():
     return "Hello World"
 
 @app.route("/contato")
-def hello():
-    return "Paulo Henrique Soares. Estou Aqui"
+def contato():
+    return """
+    <html>
+        <head>
+            <title>Contatos</title>
+        </head>
+        <body>
+            <div style='border: 2px solid red; padding: 10px; border-radius: 8px; background-color: teal;text-align: center;'>
+                <h1>Paulo Henrique Soares. Estou Aqui</h1>
+            </div>
+        </body>
+    </html>
+    """
+    
